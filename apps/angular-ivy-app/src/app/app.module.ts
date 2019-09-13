cd10e1dffe089f19af662f9684fcfd82dc36a09f
Richard Gunning <richard.gunning@frantic.com>
2019-09-13 09:06:51 +0300

    angular ivy and angular non-ivy apps

--- apps/angular-ivy-app/src/app/app.module.ts
+++ apps/angular-ivy-app/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AngularWrapperModule } from '@hybrid/angular-wrappers';
-
+import { AngularComponentLibraryModule } from '@hybrid/angular-component-library'
+//import { AngularWrapperModule } from '@hybrid/angular-wrappers'
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AngularWrapperModule
+    AngularComponentLibraryModule,
+    //AngularWrapperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
